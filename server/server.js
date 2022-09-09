de2157feed4601df8784c80dff0efccd17ce57db
Nokeen <cordrepayne@gmail.com>
2022-09-09 13:59:53 -0500

    set up server connecting with mongodb

--- server/server.js
+++ server/server.js
@@ -1,13 +1,6 @@
-const app = require('express')();
-const PORT = 3001;
+const app = require("./app");
 
-app.listen(
-    PORT,
-    () => console.log(`Server is live on http://localhost:${PORT}`)
-)
-
-app.get('/api', (req, res) => {
-    res.status(200).send({
-        users: ["userOne", "userTwo", "userThree", "userFour", "userFive"]
-    })
-})
+const PORT = process.env.PORT || 5005;
+app.listen(PORT, () => {
+  console.log(`Server listening on port http://localhost:${PORT}`);
+});
